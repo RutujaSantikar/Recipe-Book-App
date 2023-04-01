563e331e962ed823e11414afabea4cc9e39477cd
Rutuja Santikar <santikarrutuja@gmail.com>
2023-04-01 12:02:36 +0530

    basic setup done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes/recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
-
+export class RecipeListComponent implements OnInit {
+  recipes:Recipe[]=[
+    new Recipe('A test recipe', 'this is simply test','https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Chicken-leek-and-broccoli-rice-stir-fry-ffe0df6.jpg?quality=90&resize=556,505')
+  ];
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
 }
